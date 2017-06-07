1463fab3f41050aa3ea90186ec94a8f143b3c3c8
nma <mndvl0p@gmail.com>
2017-06-07 10:29:10 +0300

    add ask-name

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import readlineSync from 'readline-sync';
 
-const askName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}! `);
-};
-
-export default askName;
