b24cdfe8754244df007df432797bae7407330f7e
nma <mndvl0p@gmail.com>
2017-06-08 11:45:00 +0300

    fix brain-even

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
 import evenRun from '../brain-even-logic';
+import askName from '../ask-name';
 
-evenRun();
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+
+const name = askName();
+
+evenRun(name);
--- src/brain-even-logic.js
+++ src/brain-even-logic.js
@@ -1,17 +1,11 @@
 import readlineSync from 'readline-sync';
-import askName from './ask-name';
 
 const yes = 'yes';
 const no = 'no';
 const numOfQuestions = 3;
 const maxNum = 100;
 
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".');
-
-const name = askName();
-
-export default () => {
+export default (name) => {
   for (let i = 0; i < numOfQuestions; i += 1) {
     const randomNum = Math.round(Math.random() * maxNum);
     const correctAnswer = (randomNum % 2) ? no : yes;
