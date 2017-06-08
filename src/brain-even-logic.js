bf69483a2f46285795b1201cf5ec03ed12fd8515
nma <mndvl0p@gmail.com>
2017-06-08 14:01:08 +0300

    fix brain-even-logic

--- src/brain-even-logic.js
+++ src/brain-even-logic.js
@@ -5,11 +5,13 @@ const no = 'no';
 const numOfQuestions = 3;
 const maxNum = 100;
 
+const isEven = num => !(num % 2);
+
 export default (name) => {
   for (let i = 0; i < numOfQuestions; i += 1) {
-    const randomNum = Math.round(Math.random() * maxNum);
-    const correctAnswer = (randomNum % 2) ? no : yes;
-    console.log(`Question: ${randomNum}`);
+    const question = Math.round(Math.random() * maxNum);
+    const correctAnswer = isEven(question) ? yes : no;
+    console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (!correctAnswer.localeCompare(answer.toLowerCase())) {
       console.log('Correct!');
